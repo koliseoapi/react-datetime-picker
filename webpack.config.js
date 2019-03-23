e685ed0234c7f2444997d6199642634504895120
Nacho Coloma <icoloma@gmail.com>
2019-03-23 17:08:58 +0100

    Update all dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -4,41 +4,37 @@ This is needed only to check things manually using the browser.
 The automated tests use Mocha and Babel without going through Babel.
 */
 
-const webpack = require('webpack');
-const WebpackNotifierPlugin = require('webpack-notifier');
+const webpack = require("webpack");
+const WebpackNotifierPlugin = require("webpack-notifier");
 
 module.exports = {
   entry: {
-    app: './example/app.js',
+    app: "./example/app.js"
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   output: {
-    path: __dirname + '/build',
-    publicPath: '/build/',
-    filename: 'app.js' 
+    path: __dirname + "/build",
+    publicPath: "/build/",
+    filename: "app.js"
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              'react', 
-              // needed for tree shaking
-              // see https://medium.com/modus-create-front-end-development/webpack-2-tree-shaking-configuration-9f1de90f3233
-              ["es2015", { "modules": false }]
-            ],
-            plugins: ['lodash']
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/react", "@babel/env"]
+            }
           }
-        }]
+        ]
       }
     ]
   },
   plugins: [
     new webpack.NoEmitOnErrorsPlugin(),
-    new WebpackNotifierPlugin({ excludeWarnings: true, alwaysNotify: true }),
+    new WebpackNotifierPlugin({ excludeWarnings: true, alwaysNotify: true })
   ]
 };
