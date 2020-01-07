ab7eae1423639788923f2600e3980707f89cba06
Nacho Coloma <icoloma@gmail.com>
2020-01-07 22:14:17 +0100

    Remove old dependency

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ["@babel/env", "@babel/react"],
-    plugins: ["@babel/plugin-proposal-object-rest-spread"]
+    presets: ["@babel/env", "@babel/react"]
   };
 };
